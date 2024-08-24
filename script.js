dc98ced63a940c8a74d2e01b3e2cd0dccc9428c1
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-24 18:03:33 +0500

    Все работает

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 'use strict'
 
-let a = prompt("Угадай число от 1 до 100");
-
-const isNumber = function(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num)
-}
-
-
 function one(x) {
-    function two(a) {
+
+    function two() {
+
+        let a = prompt("Угадай число от 1 до 100");
+
+        const isNumber = function(num) {return !isNaN(parseFloat(num)) && isFinite(num)};
+        if (!isNumber(a) && (a !== null)) {alert("Введи число!"); return two();}     
+
+        if (a === null) {alert("Игра окончена"); return};
         
         a = Number(a);
-        if (a > x) {return "Загаданное число меньше";};
-        if (a < x) {return "Загаданное число больше"};
-        if (!isNumber(a)) {return "Введи число!"};
-        if (a === null) {return "Игра окончена"};
-        if (a === x) {return "Поздравляю, Вы угадали!!!"};
+
+        if ((a > x) && (a > 0)) {alert("Загаданное число меньше"); return two();};
+        if ((a < x) && (a > 0)) {alert("Загаданное число больше"); return two();};
+        if (a === 0) {alert("Введите число от 1 до 100"); return two();};
+        if (a === x) {alert("Поздравляю, Вы угадали!!!"); return;}; 
     }
 
     two();
-}
+};
 
 one(55);
