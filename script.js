e7125394db4fcf0dd08a600fbbd1aa8f8393886b
Kat Tyushnyakova <avchimchik@gmail.com>
2024-08-24 16:34:06 +0500

    Код не работает

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 'use strict'
 
+let a = prompt("Угадай число от 1 до 100");
+
+const isNumber = function(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num)
+}
+
+
+function one(x) {
+    function two(a) {
+        
+        a = Number(a);
+        if (a > x) {return "Загаданное число меньше";};
+        if (a < x) {return "Загаданное число больше"};
+        if (!isNumber(a)) {return "Введи число!"};
+        if (a === null) {return "Игра окончена"};
+        if (a === x) {return "Поздравляю, Вы угадали!!!"};
+    }
+
+    two();
+}
+
+one(55);
